9ccd8e0a4545bddd497d232150a70a276c9147c1
Tatsat Sawant <tatsatsawant6@gmail.com>
2024-01-28 14:41:47 +0530

    Project Initialized

---
+++ src/test/java/dev/tatsat/JobPostingApplication/JobPostingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tatsat.JobPostingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
