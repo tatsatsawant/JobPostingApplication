cb28af9d9763c8057e24c4b1c3e4bc7f9d407db8
Tatsat Sawant <tatsatsawant6@gmail.com>
2024-01-28 15:18:14 +0530

    created model

--- src/main/java/dev/tatsat/JobPostingApplication/model/PostModel.java
+++ src/main/java/dev/tatsat/JobPostingApplication/model/PostModel.java
@@ -1,4 +1,20 @@
 package dev.tatsat.JobPostingApplication.model;
 
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class PostModel {
+
+    private String position;
+    private String description;
+    private int experience;
+    private String skills[];
+
 }
